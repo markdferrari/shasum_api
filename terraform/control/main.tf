09e0917d8f4edd1dee448ad7dc2425ea7d84027d
markdferrari <markdferrari@gmail.com>
2021-04-22 16:27:58 +0100

    update domain, add versions.tf

--- terraform/control/main.tf
+++ terraform/control/main.tf
@@ -1,7 +1,7 @@
 module "api" {
-    source = "../module"
-    hosted_zone_id = ""
-    vpc_id = "vpc-a2dea3c4"
-    public_subnet_ids = ["subnet-f19013b9","subnet-6c02b10a","subnet-0cf50856"]
-    ssl_cert_arn = ""
+  source            = "../module"
+  hosted_zone_id    = "Z06327397CWW2Y5ES995"
+  vpc_id            = "vpc-a2dea3c4"
+  public_subnet_ids = ["subnet-f19013b9", "subnet-6c02b10a", "subnet-0cf50856"]
+  ssl_cert_arn      = "arn:aws:acm:eu-west-1:632700996244:certificate/f335c671-4d4b-409c-ae1f-6417a4ceac06"
 }
